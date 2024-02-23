1bcb4c6eb9c1c706188f8eaa13236550ffda3829
homi <homiketalys@gmail.com>
2024-02-24 00:18:05 +0800

    first modification

--- test.py
+++ test.py
@@ -2,65 +2,67 @@ import os
 import cv2
 import time
 import argparse
-
+from torchsummary import summary
 import torch
-import model.detector
-import utils.utils
+from model import detector
+from utils import utils
 
-if __name__ == '__main__':
-    #指定训练配置文件
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default='', 
-                        help='Specify training profile *.data')
-    parser.add_argument('--weights', type=str, default='', 
-                        help='The path of the .pth model to be transformed')
-    parser.add_argument('--img', type=str, default='', 
-                        help='The path of test image')
+def test(cfg_path,model_path,img_path,result_path,eval_type):
+    cfg = utils.load_datafile(cfg_path)
+    assert os.path.exists(model_path), "请指定正确的模型路径"
+    assert os.path.exists(img_path), "请指定正确的测试图像路径"
 
-    opt = parser.parse_args()
-    cfg = utils.utils.load_datafile(opt.data)
-    assert os.path.exists(opt.weights), "请指定正确的模型路径"
-    assert os.path.exists(opt.img), "请指定正确的测试图像路径"
-
-    #模型加载
+    # 模型加载
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = model.detector.Detector(cfg["classes"], cfg["anchor_num"], True).to(device)
-    model.load_state_dict(torch.load(opt.weights, map_location=device))
+    if eval_type == 0:
+        # 初始化模型
+        # modified
+        # model = model.detector_sp.Detector(cfg["classes"], cfg["anchor_num"], True).to(device)
+        model = detector.DetectorSp(cfg["classes"], cfg["anchor_num"], 2, separation=cfg["separation"],
+                                       separation_scale=cfg["separation_scale"]).to(device)
+        model.load_state_dict(torch.load(model_path, map_location=device))
+        # sets the module in eval node
+        model.eval()
 
-    #sets the module in eval node
-    model.eval()
-    
-    #数据预处理
-    ori_img = cv2.imread(opt.img)
-    res_img = cv2.resize(ori_img, (cfg["width"], cfg["height"]), interpolation = cv2.INTER_LINEAR) 
+        # 打印模型结构
+        summary(model, input_size=(3, cfg["height"], cfg["width"]))
+        one_norm = True
+    elif eval_type == 1:
+        model = detector.DetectorOrtTf(cfg, model_path)
+        one_norm = False
+
+    # 数据预处理
+    ori_img = cv2.imread(img_path)
+    res_img = cv2.resize(ori_img, (cfg["width"], cfg["height"]), interpolation=cv2.INTER_LINEAR)
     img = res_img.reshape(1, cfg["height"], cfg["width"], 3)
-    img = torch.from_numpy(img.transpose(0,3, 1, 2))
+    img = torch.from_numpy(img.transpose(0, 3, 1, 2))
     img = img.to(device).float() / 255.0
 
-    #模型推理
+    # 模型推理
     start = time.perf_counter()
-    preds = model(img)
+    with torch.no_grad():
+        preds = model(img)
     end = time.perf_counter()
-    time = (end - start) * 1000.
-    print("forward time:%fms"%time)
+    ftime = (end - start) * 1000.
+    print("forward time:%fms" % ftime)
 
-    #特征图后处理
-    output = utils.utils.handel_preds(preds, cfg, device)
-    output_boxes = utils.utils.non_max_suppression(output, conf_thres = 0.3, iou_thres = 0.4)
+    # 特征图后处理
+    output = utils.handel_preds(preds, cfg, device,one_norm)
+    output_boxes = utils.non_max_suppression(output, conf_thres=0.3, iou_thres=0.4)
 
-    #加载label names
+    # 加载label names
     LABEL_NAMES = []
     with open(cfg["names"], 'r') as f:
-	    for line in f.readlines():
-	        LABEL_NAMES.append(line.strip())
-    
+        for line in f.readlines():
+            LABEL_NAMES.append(line.strip())
+
     h, w, _ = ori_img.shape
     scale_h, scale_w = h / cfg["height"], w / cfg["width"]
 
-    #绘制预测框
+    # 绘制预测框
     for box in output_boxes[0]:
         box = box.tolist()
-       
+
         obj_score = box[4]
         category = LABEL_NAMES[int(box[5])]
 
@@ -68,9 +70,9 @@ if __name__ == '__main__':
         x2, y2 = int(box[2] * scale_w), int(box[3] * scale_h)
 
         cv2.rectangle(ori_img, (x1, y1), (x2, y2), (255, 255, 0), 2)
-        cv2.putText(ori_img, '%.2f' % obj_score, (x1, y1 - 5), 0, 0.7, (0, 255, 0), 2)	
+        cv2.putText(ori_img, '%.2f' % obj_score, (x1, y1 - 5), 0, 0.7, (0, 255, 0), 2)
         cv2.putText(ori_img, category, (x1, y1 - 25), 0, 0.7, (0, 255, 0), 2)
 
-    cv2.imwrite("test_result.png", ori_img)
+    cv2.imwrite(os.path.join(result_path,"test_result.png"), ori_img)
     
 
--- train.py
+++ train.py
@@ -13,26 +13,31 @@ from numpy.core.fromnumeric import shape
 
 from torchsummary import summary
 
-import utils.loss
-import utils.utils
-import utils.datasets
-import model.detector
+from utils import loss
+from utils import utils
+from utils import datasets
+from model import detector
 
 
-if __name__ == '__main__':
+def train(opt,log_and_save):
+    # modified
     # 指定训练配置文件
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default='',
-                        help='Specify training profile *.data')
-    opt = parser.parse_args()
-    cfg = utils.utils.load_datafile(opt.data)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--data', type=str, default='configs/ab_drive.data',
+    #                     help='Specify training profile *.data')
+    # opt = parser.parse_args()
+    # cfg = utils.utils.load_datafile(opt.data)
+    cfg = utils.load_datafile(opt.data)
 
     print("训练配置:")
     print(cfg)
 
+    # modified
     # 数据集加载
-    train_dataset = utils.datasets.TensorDataset(cfg["train"], cfg["width"], cfg["height"], imgaug = True)
-    val_dataset = utils.datasets.TensorDataset(cfg["val"], cfg["width"], cfg["height"], imgaug = False)
+    # train_dataset = utils.datasets.TensorDataset(cfg["train"], cfg["width"], cfg["height"], imgaug=True)
+    # val_dataset = utils.datasets.TensorDataset(cfg["val"], cfg["width"], cfg["height"], imgaug=False)
+    train_dataset = datasets.TensorDatasetSp(cfg["train"], cfg["width"], cfg["height"],cfg["label_flag"], imgaug=True)
+    val_dataset = datasets.TensorDatasetSp(cfg["val"], cfg["width"], cfg["height"],cfg["label_flag"], imgaug=False)
 
     batch_size = int(cfg["batch_size"] / cfg["subdivisions"])
     nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
@@ -40,17 +45,21 @@ if __name__ == '__main__':
     train_dataloader = torch.utils.data.DataLoader(train_dataset,
                                                    batch_size=batch_size,
                                                    shuffle=True,
-                                                   collate_fn=utils.datasets.collate_fn,
+                                                   # modified
+                                                   # collate_fn=utils.datasets.collate_fn,
+                                                   collate_fn=datasets.collate_fn,
                                                    num_workers=nw,
                                                    pin_memory=True,
                                                    drop_last=True,
                                                    persistent_workers=True
                                                    )
-    #验证集
+    # 验证集
     val_dataloader = torch.utils.data.DataLoader(val_dataset,
                                                  batch_size=batch_size,
                                                  shuffle=False,
-                                                 collate_fn=utils.datasets.collate_fn,
+                                                 # modified
+                                                 # collate_fn=utils.datasets.collate_fn,
+                                                 collate_fn=datasets.collate_fn,
                                                  num_workers=nw,
                                                  pin_memory=True,
                                                  drop_last=False,
@@ -60,21 +69,39 @@ if __name__ == '__main__':
     # 指定后端设备CUDA&CPU
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    # modified
     # 判断是否加载预训练模型
+    # load_param = False
+    # premodel_path = cfg["pre_weights"]
+    # if premodel_path != None and os.path.exists(premodel_path):
+    #     load_param = True
     load_param = False
     premodel_path = cfg["pre_weights"]
-    if premodel_path != None and os.path.exists(premodel_path):
-        load_param = True
+    if premodel_path == "None":
+        load_param = 2
+    elif premodel_path == 'pretrain':
+        load_param = 0
+    elif os.path.exists(premodel_path):
+        load_param = 1
 
+    # modified
     # 初始化模型结构
-    model = model.detector.Detector(cfg["classes"], cfg["anchor_num"], load_param).to(device)
+    # model = model.detector.Detector(cfg["classes"], cfg["anchor_num"], load_param).to(device)
+    model = detector.DetectorSp(cfg["classes"], cfg["anchor_num"], load_param, separation=cfg["separation"],
+                       separation_scale=cfg["separation_scale"]).to(device)
     summary(model, input_size=(3, cfg["height"], cfg["width"]))
 
+    # modified
     # 加载预训练模型参数
-    if load_param == True:
-        model.load_state_dict(torch.load(premodel_path, map_location=device), strict = False)
+    # if load_param ==True:
+    #     model.load_state_dict(torch.load(premodel_path, map_location=device), strict=False)
+    #     print("Load finefune model param: %s" % premodel_path)
+    # else:
+    #     print("Initialize weights: model/backbone/backbone.pth")
+    if load_param == 1:
+        model.load_state_dict(torch.load(premodel_path, map_location=device), strict=False)
         print("Load finefune model param: %s" % premodel_path)
-    else:
+    elif load_param == 0:
         print("Initialize weights: model/backbone/backbone.pth")
 
     # 构建SGD优化器
@@ -104,18 +131,20 @@ if __name__ == '__main__':
             # 模型推理
             preds = model(imgs)
             # loss计算
-            iou_loss, obj_loss, cls_loss, total_loss = utils.loss.compute_loss(preds, targets, cfg, device)
+            # modified
+            # iou_loss, obj_loss, cls_loss, total_loss = utils.loss.compute_loss(preds, targets, cfg, device)
+            iou_loss, obj_loss, cls_loss, total_loss = loss.compute_loss(preds, targets, cfg, device)
 
             # 反向传播求解梯度
             total_loss.backward()
 
-            #学习率预热
+            # 学习率预热
             for g in optimizer.param_groups:
-                warmup_num =  5 * len(train_dataloader)
+                warmup_num = 5 * len(train_dataloader)
                 if batch_num <= warmup_num:
-                    scale = math.pow(batch_num/warmup_num, 4)
+                    scale = math.pow(batch_num / warmup_num, 4)
                     g['lr'] = cfg["learning_rate"] * scale
-                    
+
                 lr = g["lr"]
 
             # 更新模型参数
@@ -125,23 +154,28 @@ if __name__ == '__main__':
 
             # 打印相关信息
             info = "Epoch:%d LR:%f CIou:%f Obj:%f Cls:%f Total:%f" % (
-                    epoch, lr, iou_loss, obj_loss, cls_loss, total_loss)
+                epoch, lr, iou_loss, obj_loss, cls_loss, total_loss)
             pbar.set_description(info)
 
             batch_num += 1
 
         # 模型保存
-        if epoch % 10 == 0 and epoch > 0:
+        if epoch % 10 == 0 and epoch>0:
             model.eval()
-            #模型评估
+            # 模型评估
             print("computer mAP...")
-            _, _, AP, _ = utils.utils.evaluation(val_dataloader, cfg, model, device)
+            # modified
+            # _, _, AP, _ = utils.utils.evaluation(val_dataloader, cfg, model, device)
+            _, _, AP, _ = utils.evaluation(val_dataloader, cfg, model, device,cfg["conf_thr"],nms_thresh=cfg["nms_thr"],iou_thres=cfg["iou_thr"])
             print("computer PR...")
-            precision, recall, _, f1 = utils.utils.evaluation(val_dataloader, cfg, model, device, 0.3)
-            print("Precision:%f Recall:%f AP:%f F1:%f"%(precision, recall, AP, f1))
+            # modified
+            # precision, recall, _, f1 = utils.utils.evaluation(val_dataloader, cfg, model, device, 0.3)
+            precision, recall, _, f1 = utils.evaluation(val_dataloader, cfg, model, device, 0.3,nms_thresh=cfg["nms_thr"],iou_thres=cfg["iou_thr"])
+            # modified
+            # print("Precision:%f Recall:%f AP:%f F1:%f" % (precision, recall, AP, f1))
+            log_and_save(precision,recall,AP,f1,epoch,model,cfg)
 
-            torch.save(model.state_dict(), "weights/%s-%d-epoch-%fap-model.pth" %
-                      (cfg["model_name"], epoch, AP))
+
 
         # 学习率调整
         scheduler.step()
